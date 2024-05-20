0d0694c43b8f40704df7bd94a462e6b386293def
DimiConstantin <168590061+DimiConstantin@users.noreply.github.com>
2024-05-20 18:16:09 +0300

    cu nod_max

--- posts.h
+++ posts.h
@@ -1,11 +1,13 @@
 #ifndef POSTS_H
 #define POSTS_H
 
+
 /**
  * Function that handles the calling of every command from task 2
  *
  * Please add any necessary parameters to the functions
 */
-void handle_input_posts(char *input);
+//const int nod_max = 500;
+void handle_input_posts(char *input, post_t **posts, int *nrp);
 
 #endif // POSTS_H
