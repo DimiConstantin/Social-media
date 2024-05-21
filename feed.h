99f60d3fee3c021bfedff7b852e81533d086ffa4
DimiConstantin <168590061+DimiConstantin@users.noreply.github.com>
2024-05-21 16:00:07 +0300

    Update feed.h final

--- feed.h
+++ feed.h
@@ -1,11 +1,11 @@
 #ifndef FEED_H
 #define FEED_H
-
+#include "data_structures.h"
 /**
  * Function that handles the calling of every command from task 3
  *
  * Please add any necessary parameters to the functions
 */
-void handle_input_feed(char *input);
+void handle_input_feed(char *input, post_t **posts, list_graph_t *users, int *nrp);
 
 #endif // FEED_H
