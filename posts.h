b2959d3b7f8eb785b9f1c2daf90c9cdf0ffd9d67
aeliusd <152704884+aeliusd@users.noreply.github.com>
2024-05-24 20:09:14 +0300

    Update posts.h

--- posts.h
+++ posts.h
@@ -1,13 +1,12 @@
 #ifndef POSTS_H
 #define POSTS_H
 
-
 /**
  * Function that handles the calling of every command from task 2
  *
  * Please add any necessary parameters to the functions
-*/
-//const int nod_max = 500;
-void handle_input_posts(char *input, post_t **posts, int *nrp);
+ */
+#define NOD_MAX 500
+void handle_input_posts(char* input, post_t** posts, int* nrp);
 
 #endif // POSTS_H
